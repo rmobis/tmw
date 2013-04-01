5283ac6775e67887896ffc9848907193f7954d81
Raphael Mobis Tacla <r.mobis@gmail.com>
2013-03-31 21:56:24 -0300

    Add a workaround so I can update from my other server

--- app/controllers/CharacterController.php
+++ app/controllers/CharacterController.php
@@ -6,7 +6,8 @@ class CharacterController extends BaseController {
 		// If the request does not come from ourselves, we abort it; no one
 		// should be able to update the characters from outside the server, as
 		// that could lead to several attacks.
-		if ($_SERVER['SERVER_ADDR'] !== $_SERVER['REMOTE_ADDR']) {
+		if ($_SERVER['SERVER_ADDR'] !== $_SERVER['REMOTE_ADDR'] and
+			$_SERVER['REMOTE_ADDR'] !== '66.147.244.90') {
 			return App::abort('401', 'You are not authorized to view this page.');
 		}
 
